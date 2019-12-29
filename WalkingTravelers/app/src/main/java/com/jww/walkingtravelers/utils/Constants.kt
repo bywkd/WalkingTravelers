073e78e0f1b89a33d330f578a386f6a081c0e10e
donuts_van <van@donutskorea.com>
2019-12-29 17:01:20 +0900

    WT-0007: Best페이지 mid ViewPager 무한 로딩 수정 완료

---
+++ WalkingTravelers/app/src/main/java/com/jww/walkingtravelers/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.jww.walkingtravelers.utils
+
+class Constants {
+    companion object {
+        const val BEST_IMAGE_INFINITE_COUNT: Int = 1000
+    }
+}
--- WalkingTravelers/app/src/main/java/com/jww/walkingtravelers/viewModel/BestFragmentVM.kt
+++ WalkingTravelers/app/src/main/java/com/jww/walkingtravelers/viewModel/BestFragmentVM.kt
@@ -3,23 +3,19 @@ package com.jww.walkingtravelers.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.viewpager.widget.ViewPager
 import com.jww.walkingtravelers.adapter.BestMidPagerAdapter
+import com.jww.walkingtravelers.contract.BestFragmentCon
 
 class BestFragmentVM(
-    testImages: ArrayList<Int>,
-    testCategory: ArrayList<Int>
+    private val testImages: ArrayList<Int>,
+    testCategory: ArrayList<Int>,
+    private val contract: BestFragmentCon
 ) : ViewModel() {
 
     val adapter: BestMidPagerAdapter = BestMidPagerAdapter(testCategory)
-    private val testImages = testImages
     var currentPosition: Int = 0
 
     var pageChangeListener = object : ViewPager.OnPageChangeListener {
         override fun onPageScrollStateChanged(state: Int) {
-
-            if(state == ViewPager.SCROLL_STATE_IDLE)
-            {
-
-            }
         }
 
         override fun onPageScrolled(
@@ -30,17 +26,15 @@ class BestFragmentVM(
         }
 
         override fun onPageSelected(position: Int) {
-            currentPosition = when (position) {
-                0 -> adapter.count
-                adapter.count + 1 -> 1
-                else -> position - 1
-            }
+            currentPosition = position
+            contract.setPageChange(currentPosition)
         }
     }
 
     fun setImage() {
         adapter.setImageList(testImages)
-        currentPosition = testImages.size / 2
+        currentPosition = adapter.count / 2
+        contract.setPageChange(currentPosition)
     }
 
 }
