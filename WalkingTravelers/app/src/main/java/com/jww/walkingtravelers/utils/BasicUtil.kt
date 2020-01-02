b9574852b657bbcb68822d3303ace2689bab8b6b
donuts_van <van@donutskorea.com>
2020-01-02 23:38:04 +0900

    WT-0011: 카메라 찍은후 출력 부분 수정

--- WalkingTravelers/app/src/main/java/com/jww/walkingtravelers/utils/BasicUtil.kt
+++ WalkingTravelers/app/src/main/java/com/jww/walkingtravelers/utils/BasicUtil.kt
@@ -4,8 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Environment
 import android.provider.MediaStore
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -13,23 +13,23 @@ import java.util.*
 class BasicUtil {
 
     companion object {
-        fun callCamera(activity: Activity): String {
+        fun callCamera(fragment: Fragment): String {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            intent.resolveActivity(activity.packageManager)?.let {
+            intent.resolveActivity(fragment.activity!!.packageManager)?.let {
 
                 var photoFile: File? = null
                 try {
-                    photoFile = createImageFile(activity)
+                    photoFile = createImageFile(fragment.activity!!)
                     val imageFilePath = photoFile.absolutePath
 
-                    photoFile?.let {
+                    photoFile.let {
                         val photoURI = FileProvider.getUriForFile(
-                            activity,
-                            activity.packageName + ".provider",
+                            fragment.context!!,
+                            "${fragment.activity!!.packageName}.provider",
                             it
                         )
                         intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                        activity.startActivityForResult(intent, Constants.REQUEST_CAPTURE_IMAGE)
+                        fragment.startActivityForResult(intent, Constants.REQUEST_CAPTURE_IMAGE)
                         return imageFilePath
                     }
 
@@ -42,7 +42,7 @@ class BasicUtil {
 
         fun createImageFile(activity: Activity): File {
             val timeStamp = SimpleDateFormat("yyyMMdd_HHmm", Locale.getDefault()).format(Date())
-            val imageFileName = "IMG_" + timeStamp + "_"
+            val imageFileName = "IMG_${timeStamp}_"
             val storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
             val image = File.createTempFile(imageFileName, ".jpg", storageDir)
             return image
